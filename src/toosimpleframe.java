911c3813d312f9952b596d0555ea02a0e857dbbb
Valeria943 <82813302+Valeria943@users.noreply.github.com>
2022-04-13 15:04:28 +0900

    Initial commit

---
+++ src/toosimpleframe.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class toosimpleframe extends Frame {
+    public static void main(String[] args) {
+        Frame fr = new toosimpleframe();
+        fr.setSize(400, 150);
+        fr.setVisible(true);
+    }
+}
